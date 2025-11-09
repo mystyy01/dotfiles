706ab651ceb10654860510e67ceaa6003b1f0437
mystyy01 <x0amified@gmail.com>
2025-11-09 18:39:17 +0000

    changed wallpaper and changed other config stuff

--- hypr/scripts/rofi-ai.sh
+++ hypr/scripts/rofi-ai.sh
@@ -5,7 +5,7 @@ prompt=$(rofi -dmenu -p "Ask AI:")
 [ -z "$prompt" ] && exit
 
 # Call Python script
-answer=$(python3 ~/ai_query.py "$prompt")
+answer=$(~/Coding/Spotlight-AI/venv/bin/python ~/Coding/Spotlight-AI/gpt-5-nano.py "$prompt")
 
 # Show answer in a second Rofi window
 echo "$answer" | rofi -dmenu -p "Answer"
